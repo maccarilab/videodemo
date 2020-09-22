5c43e3cd2448d0bd1cd552baa6bf3c48cdb564e1
MACCARI <maccarilab@hotmail.it>
2020-09-22 11:47:20 +0200

    Aggiungere i file di progetto.

---
+++ WebAppGitDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppGitDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
